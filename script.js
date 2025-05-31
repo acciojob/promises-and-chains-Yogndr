9a61ebda8b1ddd3c6d5d7e279580a2233fe2673e
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-05-31 11:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const age=document.querySelector("#age");
+const name=document.querySelector("#name");
+const btn=document.querySelector("#btn");
+
+btn.addEventListener(click,()=>{
+	if(!age || !name){
+		alert("Please enter valid details");
+	}
+	else{
+	return new Promise((resolve,reject)=>{
+		if(age>=18){
+			setTimeout(()=>{
+				resolve(alert(`Welcome, {name}. You can vote.`))
+			},4000)
+			
+		}
+		else{
+			setTimeout(()=>{
+				reject(alert(`Oh sorry {name}. You aren't old enough.`))
+			},4000)
+			
+		}
+		
+		
+	})
+})
+	
+}
